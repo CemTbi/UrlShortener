22f59f40db7fc530f9b2879415f0c73e864bca83
CemTbi <cem.tabakli@gmail.com>
2026-05-04 17:47:06 +0200

    Refactor Url entity for improved readability and consistency in annotations

--- src/main/java/io/github/cemtbi/model/entity/Url.java
+++ src/main/java/io/github/cemtbi/model/entity/Url.java
@@ -4,7 +4,12 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,24 +21,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Url {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false, length = 2048)
-    private String originalUrl;
+	@Column(nullable = false, length = 2048)
+	private String originalUrl;
 
-    @Column(nullable = false, unique = true, length = 10)
-    private String shortCode;
+	@Column(nullable = false, unique = true, length = 10)
+	private String shortCode;
 
-    @CreationTimestamp
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+	@CreationTimestamp
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
 
-    @Column(nullable = false)
-    private long ClickCount = 0;
+	@Column(nullable = false)
+	private long clickCount = 0;
 
-    @Column(nullable = true)
-    private LocalDateTime expiresAt;
+	@Column(nullable = true)
+	private LocalDateTime expiresAt;
 
 }
