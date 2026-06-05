943b97f4677a93eeae592fc3650e289c286eb650
CemTbi <cem.tabakli@gmail.com>
2026-06-05 03:04:14 +0200

    Remove unused @NonNull annotation import from ShortUrl class

--- src/main/java/io/github/cemtbi/model/entity/ShortUrl.java
+++ src/main/java/io/github/cemtbi/model/entity/ShortUrl.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Entity
