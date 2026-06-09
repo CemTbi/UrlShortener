1cbce2508b94a989fe7335d1d426f8cdb209b2e8
CemTbi <cem.tabakli@gmail.com>
2026-06-09 08:40:44 +0200

    Implement UrlController with create, retrieve by code, and retrieve all URLs endpoints

--- src/main/java/io/github/cemtbi/urlshortener/controller/UrlController.java
+++ src/main/java/io/github/cemtbi/urlshortener/controller/UrlController.java
@@ -1,5 +1,60 @@
 package io.github.cemtbi.urlshortener.controller;
 
-public class UrlController {
+import java.net.URI;
+import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import io.github.cemtbi.urlshortener.model.dto.UrlRequest;
+import io.github.cemtbi.urlshortener.model.dto.UrlResponse;
+import io.github.cemtbi.urlshortener.model.entity.ShortUrl;
+import io.github.cemtbi.urlshortener.repository.UrlRepository;
+import io.github.cemtbi.urlshortener.service.UrlService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/urls")
+@RestController
+public class UrlController {
+	
+	private final UrlService service;
+	
+	private final UrlRepository repository;
+	
+	@PostMapping
+    public ResponseEntity<UrlResponse> createUrl(@Valid @RequestBody UrlRequest request) {
+		ShortUrl url = service.createShortUrl(request);
+		URI location = ServletUriComponentsBuilder
+						.fromCurrentRequest()
+						.path("/{code}")
+						.buildAndExpand(url.getCode())
+						.toUri();
+		return ResponseEntity
+				.created(location)
+				.body(UrlResponse.from(url));
+	}
+	
+	@GetMapping("/{code}")
+    public ResponseEntity<UrlResponse> retreiveUrl(@PathVariable String code) {
+        ShortUrl url = service.findActiveUrl(code)
+        		.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return ResponseEntity
+        		.ok(UrlResponse
+        		.from(url));
+    }
+	
+	@GetMapping()
+    public List<ShortUrl> retreiveAllUrls() {
+        return repository.findAll();
+    }
 }
