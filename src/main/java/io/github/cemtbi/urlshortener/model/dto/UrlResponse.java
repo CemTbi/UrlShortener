beeede0671ae9bf178979f69741501d24868d06e
CemTbi <cem.tabakli@gmail.com>
2026-06-16 17:34:42 +0200

    Update UrlResponse to include expiryDate in from method

--- src/main/java/io/github/cemtbi/urlshortener/model/dto/UrlResponse.java
+++ src/main/java/io/github/cemtbi/urlshortener/model/dto/UrlResponse.java
@@ -1,13 +1,16 @@
 package io.github.cemtbi.urlshortener.model.dto;
 
+import java.time.Instant;
+
 import io.github.cemtbi.urlshortener.model.entity.ShortUrl;
 
 public record UrlResponse(
         String code,
         String url,
+        Instant expiryDate,
         long clickCount
 ) {
     public static UrlResponse from(ShortUrl entity) {
-        return new UrlResponse(entity.getCode(), entity.getUrl(), entity.getClickCount());
+        return new UrlResponse(entity.getCode(), entity.getUrl(), entity.getExpiresAt(), entity.getClickCount());
   }
 }
